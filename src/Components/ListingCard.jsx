6345bc5ef99ad2c36c601183e5f32b48b95ae22a
junyaQ <junyaqiao@gmail.com>
2024-03-14 02:37:39 -0400

    main page format

--- src/Components/ListingCard.jsx
+++ src/Components/ListingCard.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import { Card, ListGroup } from 'react-bootstrap';
 import { RiDeleteBinLine, RiSettings2Fill } from "react-icons/ri";
 import '../app/global.css';
 
-function ListingCard() {
+function ListingCard({title, city, state, country, features, amenities, safety}) {
+   
     return (
        
-        <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2'> 
+        <div> 
             <Card>
                 <Card.Img variant="top" src={require(`../imgs/house1.jpeg`)}/>
                 <Card.Body>
-                    <Card.Title>Sample Property Name</Card.Title>
+                    <Card.Title>{title}</Card.Title>
                     <Card.Text>
-                        Address: Property address
+                        Address: {city},{state},{country}
                     </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                    <ListGroup.Item>Some features: a, b, c</ListGroup.Item>
-                    <ListGroup.Item>Some amenities: d, e, f</ListGroup.Item>
-                    <ListGroup.Item>Some Safety: g, h, i</ListGroup.Item>
+                    <ListGroup.Item>Some features: {features}...</ListGroup.Item>
+                    <ListGroup.Item>Some amenities: {amenities} ...</ListGroup.Item>
+                    <ListGroup.Item>Some Safety: {safety} ...</ListGroup.Item>
                 </ListGroup>
                 <Card.Body className="flex justify-between">
                     <Card.Link href="/uploadlisting/title" className='text-blue-800'><RiSettings2Fill/></Card.Link>
--- src/Pages/LandlordSection/LandlordPage.jsx
+++ src/Pages/LandlordSection/LandlordPage.jsx
@@ -1,24 +1,45 @@
-import React from "react";
-import { Container} from "react-bootstrap";
+import React,{useState,useEffect} from "react";
+
 
 import ListingCard from "../../Components/ListingCard";
 
 
 function LandlordPage(){
+    const [property, setProperty] = useState([]);
+
+    //local
+    useEffect(() => {
+        fetch('http://localhost:3001/property'||'api/property')
+        .then(response => response.json())
+        .then(data => setProperty(data))
+        .catch(error => console.error('Error:', error));
+    }, []);
     return(
 
         <div>
-      
-        <Container className="container">
-           {/* Will use mapping function when connecting to database */}
-            <ListingCard/>
-            <ListingCard/>
-            <ListingCard/>
-            <ListingCard/>
-            <ListingCard/>
-            <ListingCard/>
-        </Container>
-</div>
+            <div className="grid  grid-col-1 md:grid-cols-3 gap-5">
+           {property.map((item, index) => (
+            <div className="min-h-0 md:max-lg:min-h-full">
+        <ListingCard
+          key={index}
+          title={item.title}
+          city={item.address.city}
+          state = {item.address.state}
+          country = {item.address.country}
+          features = {item.features.slice(0,2).map(a=>(
+            <p className="sameline">{a},</p>
+          ))}
+          amenities = {item.amenities.slice(0,2).map(a=>(
+            <p className="sameline">{a},</p>
+          ))}
+          safety = {item.safety.slice(0,2).map(a=>(
+            <p className="sameline">{a},</p>
+          ))}
+        />
+        </div>
+        ))}
+        </div>
+    </div>
     )
 }
 export default LandlordPage;
